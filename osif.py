454cc4ac4c7eaa912dfcced1e7f94cb81a47204f
tnrat-ra <75009328+tnrat-ra@users.noreply.github.com>
2020-11-27 14:10:35 +0530

    Update osif.py

--- osif.py
+++ osif.py
@@ -73,14 +73,14 @@ def show_program():
                     %sINFORMATION%s
  ------------------------------------------------------
 
-    Author     Debby Anggraini 'CiKu370'
+    Author     Prince Yasin
     Name       OSIF 'Open Source Information Facebook'
-    CodeName   D3b2y
+    CodeName   White Tiger Prince Yasin
     version    full version
     Date       16/05/2018 09:35:12
-    Team       Blackhole Security
-    Email      xnver404@gmail.com
-    Telegram   @CiKu370
+    Team       Prince Squad
+
+
 
 * if you find any errors or problems , please contact
   author
